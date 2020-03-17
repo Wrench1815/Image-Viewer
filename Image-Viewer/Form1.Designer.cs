3c4065c457c5e2fd10a80e59205d2ecbd6f90c7f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2020-03-17 21:46:05 +0530

    Added Menu to the App(Form) From now on i will refer it to app instead of Form as it is taking shape now

--- Image-Viewer/Form1.Designer.cs
+++ Image-Viewer/Form1.Designer.cs
@@ -30,56 +30,101 @@
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.ExitButton = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.changeBackgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.clearImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.tableLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.240949F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 92.75905F));
-			this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
-			this.tableLayoutPanel1.Controls.Add(this.ExitButton, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 2;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.49776F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.502242F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.666667F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.33334F));
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(801, 450);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pictureBox1.Location = new System.Drawing.Point(60, 3);
+			this.pictureBox1.Location = new System.Drawing.Point(3, 32);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(738, 414);
+			this.pictureBox1.Size = new System.Drawing.Size(795, 415);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
-			// ExitButton
+			// menuStrip1
 			// 
-			this.ExitButton.Dock = System.Windows.Forms.DockStyle.Right;
-			this.ExitButton.Location = new System.Drawing.Point(723, 423);
-			this.ExitButton.Name = "ExitButton";
-			this.ExitButton.Size = new System.Drawing.Size(75, 24);
-			this.ExitButton.TabIndex = 2;
-			this.ExitButton.Text = "Exit";
-			this.ExitButton.UseVisualStyleBackColor = true;
+			this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuToolStripMenuItem
+			// 
+			this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem,
+            this.changeBackgroundColorToolStripMenuItem,
+            this.clearImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+			this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+			this.menuToolStripMenuItem.Text = "Menu";
+			// 
+			// openImageToolStripMenuItem
+			// 
+			this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+			this.openImageToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+			this.openImageToolStripMenuItem.Text = "Open Image";
+			// 
+			// changeBackgroundColorToolStripMenuItem
+			// 
+			this.changeBackgroundColorToolStripMenuItem.Name = "changeBackgroundColorToolStripMenuItem";
+			this.changeBackgroundColorToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+			this.changeBackgroundColorToolStripMenuItem.Text = "Change Background Color";
+			// 
+			// clearImageToolStripMenuItem
+			// 
+			this.clearImageToolStripMenuItem.Name = "clearImageToolStripMenuItem";
+			this.clearImageToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+			this.clearImageToolStripMenuItem.Text = "Clear Image";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Form1";
 			this.Text = "Image Viewer";
 			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -88,7 +133,12 @@
 
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.Button ExitButton;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem changeBackgroundColorToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem clearImageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
 
